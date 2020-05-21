9fa9d6e553360fe1d59ce94c5a08698b329ba6f7
nayara.pimentel <n.pimentel@vallourec.com>
2020-05-20 22:20:38 -0300

    Pratica 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DatasComponent } from './datas/datas.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EnderecoComponent } from './endereco/endereco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatasComponent,
+    EventosComponent,
+    EnderecoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/endereco/endereco.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco',
+  templateUrl: './endereco.component.html',
+  styleUrls: ['./endereco.component.css']
+})
+export class EnderecoComponent implements OnInit {
+
+  @Input() location;
+  @Input() nome;
+  @Output() atualizaEndereco = new EventEmitter();
+
+  constructor() { }
+
+  atualizarEndereco(local, cidade, pais) {
+    this.atualizaEndereco.emit(
+      {
+        name: this.nome,
+        location: {
+          address: local,
+          city: cidade,
+          country: pais
+        }
+      });
+  }
+
+  ngOnInit(): void {
+  }
+}
